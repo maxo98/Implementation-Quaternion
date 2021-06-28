42848323c258766edc712cb25d5ee41ba795a871
erwan-ash <60441163+erwan-ash@users.noreply.github.com>
2021-06-28 15:41:07 +0200

    first commit

---
+++ Assets/Caracter_move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caracter_move : MonoBehaviour
+{
+    public CharacterController myController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            myController.Move(transform.forward);
+        }
+        
+    }
+}
